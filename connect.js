340281a28d2d251b12dde5215d6aa171118cd1fb
Chad <chadghobara@gmail.com>
2023-03-28 15:41:25 +0200

    createIndex

--- connect.js
+++ connect.js
@@ -5,20 +5,64 @@ const url = "mongodb+srv://maxime:t2im3wo7reBU7DoW@cluster0.0e8gtjr.mongodb.net/
 const client = new MongoClient(url);
 
 async function run() {
-    try {
+
         await client.connect();
         console.log("TEST ?");
         const db = client.db('Taches');
         const collection = db.collection('documents');
-        const insertStuff = await collection.insertMany(
-            [{a : 1}, {b : 2}, {c : 3},]
-        );
-        console.log(`Docouments inseres => ${insertStuff}`);
+   // CREATE
+        try {
+            const insertData = await collection.insertMany([
+                {
+                name : 'Alex',
+                age : '30',
+                ville : 'Paris'
+                },
+                {
+                    name : 'Marie',
+                    age : '25',
+                    ville : 'Lille'
+                },
+                {
+                    name : 'Bruno',
+                    age : '55',
+                    ville : 'Marseille'
+                    },
+                
+]);
+        console.log('Docouments inseres =>', insertData);
         return 'done'
-
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err.stack);
     }
+    //
+    // READ
+    try {
+
+    }
+    catch (err) {
+        console.log(err.stack);
+    }
+    //
+    // DELETE
+    try {
+
+
+    }
+    catch (err) {
+        console.log(err.stack);
+    }
+    //
+    // UPDATE
+    try {
+
+    }
+    catch (err) {
+        console.log(err.stack);
+    }
+    
+    
     finally {
         await client.close();
     }
